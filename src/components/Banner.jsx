48702e1ed0be2e306b4b7b52d70195d061373553
Jebrel Esmail <jebrelesmail@Jebrels-MacBook-Pro.local>
2024-03-21 20:30:05 -0400

    Orbit-Report

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import "./styling.css"
+
+const Banner = () => {
+    return (
+        <h1>Orbit Report</h1>
+    )
+}
+
+export default Banner;
+
+
+{/* <div>
+<p>Click on the buttons to see the satellites in that orbit type</p>
+</div> */}
